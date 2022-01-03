2e75229cad0a05cad53f7d9dfc67868681341d9a
rory <rory@vibeiq.com>
2022-01-03 11:02:31 -0500

    feat / plan app widget

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
 import React, { useEffect, useState } from "react";
-import logo from './logo.svg';
 import './App.css';
 const Render = () => {
-  const [state, setState] = useState("Initial State");
+  // const stateRef = React.useRef([]);
+  const [state, setState] = useState([]);
   const [output, setOutput] = useState('')
 
   const onClick = () => {
-    setState("Clicked");
+    setState([{ id: '0000001' }]);
   };
   const toParent = (e) => {
     if (window && window.parent) {
       e.preventDefault();
-      window.parent.postMessage({content: output}, 'http://localhost:4200');
+      window.parent.postMessage({ content: output }, 'http://localhost:4201');
     }
   };
 
   const onMessageReceivedFromIframe = React.useCallback(
     event => {
-      console.log("FromParent---", state, '===', typeof(event.data), event.data);
-      if (event.origin.startsWith('http://localhost:4200')) {
+      // if (event.origin.startsWith('http://localhost:4201')) {
+      if (event.data.app === 'vibeiq') {
         setState(event.data.content)
+        console.log("FromParent---", event.data, state);
+        // stateRef.current = event.data.content;
       }
-      window.removeEventListener("message", onMessageReceivedFromIframe);
     },
     [state]
   );
@@ -31,44 +32,35 @@ const Render = () => {
     return () => window.removeEventListener("message", onMessageReceivedFromIframe);
   }, [onMessageReceivedFromIframe]);
 
-  // React.useEffect(() => {
-  //   console.log("state =======", state);
-  // }, [state]);
-
   const handleInputChange = (event) => {
     setOutput(event.target.value);
   }
 
   return (
-    <div>
-      <p>{state}</p>
+    <div style={{paddingTop: 12}}>
+      {state.map((rowData) =>
+        <div key={rowData.id} className="row-data">
+          <span>Id: {rowData?.id}</span>
+          <span>Name: {rowData?.name}</span>
+          <span>Option Name: {rowData?.optionName}</span>
+          <span>Target Volume: {rowData?.targetVolume}</span>
+        </div>
+      )}
+
+      {/* <p>{state}</p>
       <button onClick={onClick}>Click here</button>
       <p>{output || 'Input Value here'}</p>
       <input type="text" onChange={handleInputChange} />
-      <button onClick={toParent}>To Parent</button>
+      <button onClick={toParent}>To Parent</button> */}
     </div>
   )
-  
+
 };
 
 function App() {
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Render />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-
-    </div>
+    <Render />
   );
 }
 
